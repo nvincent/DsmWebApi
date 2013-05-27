32b6ad902d0f9f1a86bb7863fc9aeddeca81c5b2
Nicolas VINCENT <nvincent@server.fake>
2013-05-27 22:27:07 +0200

    Added Unit Tests in the Core project for the AuthenticationApi, DsmApiInfo and DsmApiResponse classes.

---
+++ DsmWebApi.Core.Tests/AuthenticationApiUnitTest.cs
@@ -0,0 +1,67 @@
+﻿namespace DsmWebApi.Core.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Unit tests for the class <see cref="AuthenticationApi"/>.
+    /// </summary>
+    [TestClass]
+    public class AuthenticationApiUnitTest : DsmApiBaseUnitTest
+    {
+        /// <inheritdoc />
+        protected override DsmApiBase DsmApiBase
+        {
+            get { return this.AuthenticationApi; }
+        }
+
+        /// <summary>
+        /// Gets or sets the authentication API to test.
+        /// </summary>
+        private AuthenticationApi AuthenticationApi { get; set; }
+
+        /// <summary>
+        /// Runs once before all tests in the class.
+        /// </summary>
+        /// <param name="testContext">The test context.</param>
+        [ClassInitialize]
+        public static new void MyClassInitialize(TestContext testContext)
+        {
+            DsmApiBaseUnitTest.MyClassInitialize(testContext);
+        }
+
+        /// <summary>
+        /// Runs before each test in the class.
+        /// </summary>
+        [TestInitialize]
+        public override void MyTestInitialize()
+        {
+            base.MyTestInitialize();
+            this.AuthenticationApi = new AuthenticationApi(this.DsmApiContext);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="M:AuthenticationApi.AuthenticationApi(IDsmApiContext)"/> constructor.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestConstructorArgumentNullException()
+        {
+            var authenticationApi = new AuthenticationApi(null);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="M:AuthenticationApi.LogOn"/> method.
+        /// </summary>
+        [TestMethod]
+        public void TestLogOn()
+        {
+            var authenticationApi = this.AuthenticationApi;
+            Assert.IsNotNull(authenticationApi);
+
+            DsmApiResponse response = authenticationApi.LogOn("admin", "admin").Result;
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.Success);
+        }
+    }
+}
---
+++ DsmWebApi.Core.Tests/DsmApiInfoUnitTest.cs
@@ -0,0 +1,69 @@
+﻿namespace DsmWebApi.Core.Tests
+{
+    using System;
+    using Microsoft.Practices.ServiceLocation;
+    using Microsoft.Practices.Unity;
+    using Microsoft.Practices.Unity.Configuration;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// Unit tests for the class <see cref="DsmApiInfo"/>.
+    /// </summary>
+    [TestClass]
+    public class DsmApiInfoUnitTest
+    {
+        /// <summary>
+        /// Runs once before all tests in the class.
+        /// </summary>
+        /// <param name="testContext">The test context.</param>
+        [ClassInitialize]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            ServiceLocator.SetLocatorProvider(() => new UnityServiceLocator(new UnityContainer().LoadConfiguration()));
+        }
+
+        /// <summary>
+        /// Tests the <see cref="M:DsmApiInfo.DsmApiInfo(string, string, int, int)"/> constructor.
+        /// </summary>
+        [TestMethod]
+        public void TestConstructor()
+        {
+            var dsmApiInfo = new DsmApiInfo("name", "path", 3, 12);
+            Assert.AreEqual("name", dsmApiInfo.Name);
+            Assert.AreEqual("path", dsmApiInfo.Path);
+            Assert.AreEqual(3, dsmApiInfo.MinVersion);
+            Assert.AreEqual(12, dsmApiInfo.MaxVersion);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="M:DsmApiInfo.ConvertFrom"/> method.
+        /// </summary>
+        [TestMethod]
+        public void TestConvertFrom()
+        {
+            JProperty property = new JProperty(
+                "name",
+                new JObject(
+                    new JProperty("path", "path"),
+                    new JProperty("name", "name"),
+                    new JProperty("minVersion", 3),
+                    new JProperty("maxVersion", 12)));
+            var dsmApiInfo = DsmApiInfo.ConvertFrom(property);
+            Assert.AreEqual("name", dsmApiInfo.Name);
+            Assert.AreEqual("path", dsmApiInfo.Path);
+            Assert.AreEqual(3, dsmApiInfo.MinVersion);
+            Assert.AreEqual(12, dsmApiInfo.MaxVersion);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="M:DsmApiInfo.ConvertFrom"/> method.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestConvertFromArgumentNullException()
+        {
+            var dsmApiInfo = DsmApiInfo.ConvertFrom(null);
+        }
+    }
+}
