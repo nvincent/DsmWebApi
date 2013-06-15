7fb7b20768dcee7ba7ed8e72bc25967b931e7f4a
Nicolas VINCENT <nvincent@server.fake>
2013-06-15 19:34:54 +0200

    Added a WPF converter to convert empty strings back to null values.

---
+++ DsmWebApi.WpfClient/EmptyStringToNullConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace DsmWebApi.WpfClient
+{
+    using System;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// Converts empty strings back to a null value.
+    /// </summary>
+    internal class EmptyStringToNullConverter : IValueConverter
+    {
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == (object)string.Empty)
+            {
+                return null;
+            }
+
+            return value;
+        }
+    }
+}
